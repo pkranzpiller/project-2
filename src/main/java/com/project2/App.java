18bbc680efe255d463277a1053fcb5b45870afb2
Patrick Kranzpiller <pkranzpiller@gmail.com>
2019-08-14 16:33:38 -0500

    awegwg

--- src/main/java/com/project2/App.java
+++ src/main/java/com/project2/App.java
@@ -4,8 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 public class App {
 
